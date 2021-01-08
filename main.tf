6cff2f5b28e81a7d77532cae5613e09bb76bc0a2
Sumeet Ninawe <sumeet.ninawe@gmail.com>
2021-01-08 17:05:44 +0100

    excluding provider information

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-1"
-}
-
 resource "aws_instance" "demo" {
   count                   = 2
   ami                     = var.ami
